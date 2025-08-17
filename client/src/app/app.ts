ff9c4d759aa178e38ba383d6b8d6f98c57f67105
AhmedAbdelhamid19 <ahmedaways200219@gmail.com>
2025-08-17 18:04:34 +0300

    updating UI,  navbar, home page

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,25 +3,38 @@ import { Component, inject, signal, OnInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from '../layout/nav/nav';
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css' 
 })
 export class App implements OnInit{
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title  = signal('Social Network');
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   async ngOnInit() {
-    this.members.set(await this.getMembers())
+    this.members.set(await this.getMembers());
+    this.setCurrentUser();
+  }
+  setCurrentUser() {
+    // if user logged in then the information stored in local storage
+    // if user not logged in then remain in as you are
+    const userString = localStorage.getItem('user');
+    if(!userString) return
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
   async getMembers() {
     try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members/getusers'));
+      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members/getusers'));
     } catch (error) {
       console.log(error);
       throw error;
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -9,26 +9,22 @@ import { AccountService } from '../../core/services/account-service';
   styleUrl: './nav.css'
 })
 export class Nav {
-  private accountService = inject(AccountService);
+  protected accountService = inject(AccountService);
   protected creds: any = {}
-  protected loggedIn = signal(false);
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         console.log(result);
-        this.loggedIn.set(true);
-        this.creds = {};cd
+        this.creds = {};
       },
       error: error => {
         alert(error.message);
-        this.loggedIn.set(false);
       }
     });
   }
 
   logout() {
-    this.creds = {}
-    this.loggedIn.set(false);
+    this.accountService.logout();
   }
 }
