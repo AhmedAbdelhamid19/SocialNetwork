6552c9f31f18f8db2b1099dfeac12462a6f1177d
AhmedAbdelhamid19 <ahmedaways200219@gmail.com>
2025-08-23 13:41:20 +0300

    adding route guards to specific buttons and enhancing UI

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,12 +3,19 @@ import { Home } from '../features/home/home';
 import { MemeberList } from '../features/members/memeber-list/memeber-list';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
 export const routes: Routes = [
     {path: '', component: Home},
-    {path: 'members', component: MemeberList},
-    {path: 'members/:id', component: MemeberList},
-    {path: 'lists', component: Lists},
-    {path: 'messages', component: Messages},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        children: [
+            {path: 'members', component: MemeberList, canActivate: [authGuard]},
+            {path: 'members/:id', component: MemeberList},
+            {path: 'lists', component: Lists},
+            {path: 'messages', component: Messages}
+        ]
+    },
     {path: '**', component: Home}
 ];
