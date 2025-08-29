3041930c70b5c6eb9bfa3d7f70462c331a8d3fa3
AhmedAbdelhamid19 <ahmedaways200219@gmail.com>
2025-08-29 14:46:07 +0300

    add Postman API collection and implement BugController
    - Added Postman JSON collection for API testing and documentation
    - Created BugController with initial CRUD endpoints

---
+++ API/Controllers/BugController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BugController : ControllerBase
+    {
+        [HttpGet("auth")] 
+        public IActionResult GetAuth() 
+        {
+            return Unauthorized();
+        }
+        [HttpGet("not-found")] 
+        public IActionResult GetNotFound() 
+        {
+            return NotFound();
+        }
+        [HttpGet("server-error")] 
+        public IActionResult GetServerError() 
+        {
+            throw new Exception("Internal Server Error happen");
+        }
+        [HttpGet("bad-request")] 
+        public IActionResult GetBadRequest() 
+        {
+            throw new Exception("This is not Good Request");
+        }
+    }
+}
